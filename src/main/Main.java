ec0f2c31e056475331056a9606d17418c36bc7a0
MasterEman <emanuel@kulich.me>
2019-01-26 17:37:59 +0100

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner scr = new Scanner(System.in);
 		System.out.println("---- Auto Mouse Clicker ----");
 		
 		System.out.println("Enter the number of desired clicks: ");
