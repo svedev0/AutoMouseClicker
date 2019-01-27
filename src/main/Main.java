22c114d46ede3aefa5d393b3354518e42e82466d
MasterEman <emanuel@kulich.me>
2019-01-27 14:25:47 +0100

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,38 +7,70 @@ public class Main {
 	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
+		
 		Scanner scr = new Scanner(System.in);
 		System.out.println("---- Auto Mouse Clicker ----");
 		
-		System.out.println("Enter the number of desired clicks: ");
+		System.out.println("Number of clicks: (0 = infinate)");
 		int clicks = scr.nextInt();
 		System.out.println();
 		
 		System.out.println("Enter delay between clicks in miliseconds: ");
-		int delay = scr.nextInt();
-		System.out.println();
-		System.out.println("Program will start in 3 seconds!");
 		
-		try {
-			Thread.sleep(3000);
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (scr.nextInt() == 0) {
+			int delay = 1000000 * 1000000;
+			
+			System.out.println();
+			System.out.println("Program will start in 3 seconds!");
+			
+			try {
+				Thread.sleep(3000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			Clicker clicker = new Clicker();
+			clicker.setDelay(delay);
+			
+			for (int i = 0; i < clicks; i++) {
+				clicker.clickMouse(InputEvent.BUTTON1_MASK);
+			}
+			
+			System.out.println("Clicking complete!");
+			try {
+				Thread.sleep(5000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.exit(0);
+			
+		} else {
+			int delay = scr.nextInt();
+			
+			System.out.println();
+			System.out.println("Program will start in 3 seconds!");
+			
+			try {
+				Thread.sleep(3000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			Clicker clicker = new Clicker();
+			clicker.setDelay(delay);
+			
+			for (int i = 0; i < clicks; i++) {
+				clicker.clickMouse(InputEvent.BUTTON1_MASK);
+			}
+			
+			System.out.println("Clicking complete!");
+			try {
+				Thread.sleep(5000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.exit(0);
 		}
-		
-		Clicker clicker = new Clicker();
-		clicker.setDelay(delay);
-		
-		for (int i = 0; i < clicks; i++) {
-			clicker.clickMouse(InputEvent.BUTTON1_MASK);
-		}
-		
-		System.out.println("Clicking complete!");
-		try {
-			Thread.sleep(5000);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.exit(0);
 	}
 }
 
